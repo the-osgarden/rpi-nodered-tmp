8912992e275028edcf6c5588bc4f11bed5fb4fbe
Geoff Oslund <geoff@ineight.nett>
2015-09-16 12:01:57 -0600

    test

--- nodered-tmp36.js
+++ nodered-tmp36.js
@@ -1,15 +1,21 @@
 var tmp36 = require('tmp36');
-
 module.exports = function(RED) {
     function piNoderedTmp(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         var node = this;
         this.on('input', function(msg) {
-            var t = new tmp36();
+            var t = new tmp36({
+                address: 120,
+                base: 48,
+                pin: 2,
+                voltage: 3.3,
+                interval: 300,
+                samples: 20,
+            });
             msg.temp = t.temperature();
             console.log(msg);
             node.send(msg);
         });
     }
-    RED.nodes.registerType("pi-nodered-tmp",piNoderedTmp);
+    RED.nodes.registerType("pi-nodered-tmp", piNoderedTmp);
 }
